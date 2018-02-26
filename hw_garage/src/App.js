152a50fc2da41a265d087bfc299504fc96f96149
Travis Meamber <tmeamber623@gmail.com>
2018-02-26 10:04:46 -0800

    css and packages

--- hw_garage/src/App.js
+++ hw_garage/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <div className='Header'>
+      <div className='Logo'>
+      </div>
+      <div className='upper'>
+      <div className='login'>Sign up / Login
+      </div>
+      </div>
+      </div>
+        <div className='Navbar'>
+        <Link to='/profile'>
+          <div className='profile'>Profile</div>
+        </Link>
+        </div>
       </div>
     );
   }
